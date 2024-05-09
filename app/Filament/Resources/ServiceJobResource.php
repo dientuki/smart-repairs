5c81f921085fcadae8d2ea635dd73ea4a569f601
Juan "dientuki" Farias <dientuki@gmail.com>
2024-05-08 22:25:59 -0300

    add service jobs

---
+++ app/Filament/Resources/ServiceJobResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ServiceJobResource\Pages;
+use App\Filament\Resources\ServiceJobResource\RelationManagers;
+use App\Models\ServiceJob;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ServiceJobResource extends Resource
+{
+    protected static ?string $model = ServiceJob::class;
+
+    protected static ?string $tenantRelationshipName = 'clients';
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                TextInput::make('price'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('price'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListServiceJobs::route('/'),
+            'create' => Pages\CreateServiceJob::route('/create'),
+            'edit' => Pages\EditServiceJob::route('/{record}/edit'),
+        ];
+    }
+}
