662fcc0fc1af046502c3cf45733336d1f402389b
Juan "dientuki" Farias <dientuki@gmail.com>
2024-05-15 22:35:52 -0300

    react alone

---
+++ board/Board.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+function Board() {
+    return (
+        <>
+            <h1>Board</h1>
+            <p>This is the board page.</p>
+            <p>It is currently empty.</p>
+
+            <p><a href="https://laravel.com/docs">Documentation</a></p>
+        </>
+    );
+}
+
+export default Board;
---
+++ board/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Board from "./Board";
+import './index.css'
+
+const appElement = document.getElementById('app');
+if (appElement) {
+    ReactDOM.createRoot(appElement).render(
+        <Board />
+    );
+}
