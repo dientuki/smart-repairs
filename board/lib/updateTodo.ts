c7af0059e3f6e221c44cdd348bbada55358658cc
Juan "dientuki" Farias <dientuki@gmail.com>
2024-05-18 17:20:48 -0300

    move card

--- board/lib/updateTodo.ts
+++ board/lib/updateTodo.ts
@@ -1,13 +1,16 @@
 export const updateTodo = async (todo: Todo, columnId: TypedColumn) => {
-    /*
-    await databases.updateDocument(
-        process.env.NEXT_PUBLIC_DATABASE_ID!,
-        process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
-        todo.$id,
-        {
-          title: todo.title,
-          status: columnId
-        }
-      );
-      */
+
+  const data = await fetch('http://localhost/graphql', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        query: `
+            mutation {
+              updateOrderStatus(id: ${todo.$id}, status: "${columnId}")
+          }
+        `
+    })
+  });
 }
