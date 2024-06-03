7fd8c814abfe0b9d04afbea45b746cad2898faf6
Juan "dientuki" Farias <dientuki@gmail.com>
2024-06-03 00:49:28 -0300

    [SM-86a3k1ty1-modal-improvement] add first order

--- board/page.tsx
+++ board/page.tsx
@@ -1,19 +1,22 @@
 import ReactDOM from 'react-dom/client';
 import Header from "@/components/Header";
 import Board from "@/components/Board";
-import AddButton from "@/components/AddButton";
-import Modal from "@/components/Modal";
-import ViewCardModal from "./components/ViewCardModal";
+import Modal from "@/components/modal/Modal";
+import { ModalContainer } from "react-modal-global";
+import { Suspense } from "react";
 import './global.css'
+import ModalLayout from "@/components/modal/ModalLayout";
 
 const appElement = document.getElementById('app');
+
 if (appElement) {
     ReactDOM.createRoot(appElement).render(
         <>
             <Header />
             <Board />
-            <AddButton />
-            <ViewCardModal />
+            <Suspense>
+                <ModalContainer controller={Modal} template={ModalLayout} />
+            </Suspense>
         </>
     );
 }
