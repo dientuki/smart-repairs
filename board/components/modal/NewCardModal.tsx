d8eea15a83f86387b325841925d3af08175c1bc1
Juan "dientuki" Farias <dientuki@gmail.com>
2024-06-07 00:42:56 -0300

    SM-86a3mn09u-add-new-order] add step3

--- board/components/modal/NewCardModal.tsx
+++ board/components/modal/NewCardModal.tsx
@@ -1,30 +1,39 @@
 import "react-modal-global/styles/modal.scss" // Imports essential styles for `ModalContainer`.
 import ModalLayout from "@/components/modal/ModalLayout";
 import { Field, Input, Label, Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
-import { Autocomplete, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import Step1 from "@/components/newCardForm/Step1";
+import Step2 from "@/components/newCardForm/Step2";
+import Step3 from "@/components/newCardForm/Step3";
 import { useOrderStore } from "@/store/OrderStore";
-
-const top100Films = [
-  { label: 'The Shawshank Redemption', year: 1994 },
-  { label: 'Inception', year: 2010 }
-];
 
 function NewCardModal() {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const { data, getData } = useOrderStore();
+  const [customer, setCustomer] = useState<string | null>(null);
+  const [device, setDevice] = useState<string | null>(null);
 
   useEffect(() => {
     getData("01HZJ9PYBNDCMQYHGCXMFHBFK3");
   }, [getData]);
 
-  const nextStep = (customerId: string) => {
-    console.log(customerId);
-    setSelectedIndex(selectedIndex + 1);
+  const goToStep2 = (customerId: string) => {
+    setCustomer(customerId);
+    nextStep();
   };
 
-  console.log(data);
+  const goToStep3 = (deviceId: string) => {
+    setDevice(deviceId);
+    nextStep();
+  };
+
+  const prevStep = () => {
+    setSelectedIndex(selectedIndex - 1);
+  };
+
+  const nextStep = () => {
+    setSelectedIndex(selectedIndex + 1);
+  };
 
   return (
     <ModalLayout>
@@ -35,45 +44,17 @@ function NewCardModal() {
           </h2>
 
           <TabGroup defaultIndex={0} selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-            <TabList>
-              <Tab className="mx-2">Informacion del cliente</Tab>
-              <Tab  className="mx-2">Informacion del equipo</Tab>
-              <Tab className="mx-2">Problema</Tab>
+            <TabList className="my-5">
+              <Tab className="mr-2 border border-gray-300 p-3">Informacion del cliente</Tab>
+              <Tab  className="mx-2 border border-gray-300 p-3">Informacion del equipo</Tab>
+              <Tab className="mx-2 border border-gray-300 p-3">Problema</Tab>
             </TabList>
             <TabPanels>
 
-              <Step1 nextStep={nextStep} customers={data.customers} />
+              <Step1 nextStep={goToStep2} customers={data.customers} />
+              <Step2 prevStep={prevStep} nextStep={goToStep3} devices={data.devices} />
+              <Step3 prevStep={prevStep} customer={customer} device={device} />
 
-              <TabPanel unmount={false}>
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo2"
-                  options={top100Films}
-                  sx={{ width: 300 }}
-                  renderInput={(params) => <TextField {...params} label="Device" />}
-                />
-                <div>IMEI si lo tengo</div>
-              <Field>
-                <Label className="">Tipo</Label>
-                <Input name="full_name" className="border border-gray-300 p-3 mt-3 block w-full rounded-lg " />
-              </Field>
-              <Field>
-                <Label className="">Marca</Label>
-                <Input name="full_name" className="border border-gray-300 p-3 mt-3 block w-full rounded-lg " />
-              </Field>
-              <Field>
-                <Label className="">Nombre comercial</Label>
-                <Input name="full_name" className="border border-gray-300 p-3 mt-3 block w-full rounded-lg " />
-              </Field>
-              <Field>
-                <Label className="">Nombre tecnico</Label>
-                <Input name="full_name" className="border border-gray-300 p-3 mt-3 block w-full rounded-lg " />
-              </Field>
-              <Field>
-                <Label className="">Web</Label>
-                <Input name="full_name" className="border border-gray-300 p-3 mt-3 block w-full rounded-lg " />
-              </Field>
-              </TabPanel>
               <TabPanel unmount={false}>
                 <Field>
                   <Label className="">Imei</Label>
---
+++ board/components/newCardForm/Step3.tsx
@@ -0,0 +1,108 @@
+import { Autocomplete, Skeleton, TextField, createFilterOptions } from "@mui/material";
+import { Field, Input, Label, TabPanel } from '@headlessui/react';
+import { Controller, useForm, FieldValues, FieldErrors } from "react-hook-form";
+import { useState } from "react";
+
+const filter = createFilterOptions<Customer>();
+type Props = {
+  prevStep: () => void,
+  customer: string | null,
+  device: string | null
+}
+
+function Step3({ customer, prevStep, device }: Props) {
+  const { handleSubmit, control, formState: { errors }, setValue } = useForm();
+
+  console.log(customer, device);
+
+  const handleRegistration = (data: FieldValues ) => {
+    /*
+    if (selectedDevice === null) return;
+    console.log(selectedDevice, data);
+    nextStep(selectedDevice.id);
+    */
+   console.log(data);
+   return;
+  };
+
+  const handleError = (errors: FieldErrors<FieldValues>) => {
+    console.log(errors);
+  };
+
+  const registerOptions = {
+    serial: { required: "First name is required" },
+    unlockType: { required: "First name is required" },
+    unlockCode: { },
+  };
+
+  return (
+    <TabPanel unmount={false}>
+      <form onSubmit={handleSubmit(handleRegistration, handleError)}>
+
+        <Field className="mt-4">
+          <Label>Imei/Serie</Label>
+          <Controller
+            name="serial"
+            defaultValue=""
+            control={control}
+            rules={registerOptions.serial}
+            render={({ field }) => (
+              <Input {...field} className="border border-gray-300 p-3 block w-full rounded-lg" />
+            )}
+          />
+          {errors?.serial && errors.serial.message && (
+            <small className="text-danger">
+              <span>{typeof errors.serial.message === 'string' ? errors.serial.message : JSON.stringify(errors.serial.message)}</span>
+            </small>
+          )}
+        </Field>
+
+        <Field className="mt-4">
+          <Label className="">Selector de desbloque</Label>
+          <Controller
+            name="unlockType"
+            control={control}
+            defaultValue=""
+            rules={registerOptions.unlockType}
+            render={({ field }) => (
+              <Input  {...field} className="border border-gray-300 p-3 block w-full rounded-lg" />
+            )}
+          />
+          {errors?.unlockType && errors.unlockType.message && (
+            <small className="text-danger">
+              <span>{typeof errors.unlockType.message === 'string' ? errors.unlockType.message : JSON.stringify(errors.unlockType.message)}</span>
+            </small>
+          )}
+
+        </Field>
+
+        <Field className="mt-4">
+          <Label className="">Codigo de desbloqueo</Label>
+          <Controller
+            name="unlockCode"
+            control={control}
+            defaultValue=""
+            rules={registerOptions.unlockCode}
+            render={({ field }) => (
+              <Input  {...field} className="border border-gray-300 p-3 block w-full rounded-lg" />
+            )}
+          />
+          {errors?.unlockCode && errors.unlockCode.message && (
+            <small className="text-danger">
+              <span>{typeof errors.unlockCode.message === 'string' ? errors.unlockCode.message : JSON.stringify(errors.unlockCode.message)}</span>
+            </small>
+          )}
+
+        </Field>
+
+        <div onClick={prevStep} className="mt-6 rounded-md bg-sky-600 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">Anterior</div>
+
+        <input type="submit" className="mt-6 rounded-md bg-sky-600 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600" />
+
+      </form>
+
+    </TabPanel>
+  )
+}
+
+export default Step3;
