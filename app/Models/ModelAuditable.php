2445a1b9e887729fdd9a58623ddacf8084b2cc11
Juan "dientuki" Farias <dientuki@gmail.com>
2024-06-12 13:33:52 -0300

    [SM-86a3qm46x-login-laravel-ui] fix ulid in uppercase

--- app/Models/ModelAuditable.php
+++ app/Models/ModelAuditable.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
@@ -10,4 +11,11 @@ class ModelAuditable extends Model implements Auditable
     use HasUlids;
 
     use \OwenIt\Auditing\Auditable;
+
+    protected function id(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => strtoupper($value),
+        );
+    }
 }
