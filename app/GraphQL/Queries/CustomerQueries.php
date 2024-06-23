5e196ddf1d9cc40f4b02dfe66b5f953a4ca51461
Juan "dientuki" Farias <dientuki@gmail.com>
2024-06-23 01:30:37 -0300

    [SM-86a3wtddm-improve-user] update in queries

--- app/GraphQL/Queries/CustomerQueries.php
+++ app/GraphQL/Queries/CustomerQueries.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App\GraphQL\Queries;
 
 use App\Models\Customer;
+use App\Traits\TeamContextTrait;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 final readonly class CustomerQueries
 {
-    public static function getCustomers()
+    use TeamContextTrait;
+
+    public function getCustomers(null $root, array $args, GraphQLContext $context): mixed
     {
-        return Customer::where('team_id', auth()->user()->teams->first()->id)->get();
+        $team_id = $this->getTeamIdFromContext($context);
+
+        return Customer::where('team_id',)->get();
     }
 }
--- app/GraphQL/Queries/DeviceUnitQueries.php
+++ app/GraphQL/Queries/DeviceUnitQueries.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace App\GraphQL\Queries;
 
 use App\Models\DeviceUnit;
+use App\Traits\TeamContextTrait;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 final readonly class DeviceUnitQueries
 {
-    public static function getDeviceUnits()
+    use TeamContextTrait;
+
+    public function getDeviceUnits(null $root, array $args, GraphQLContext $context): mixed
     {
-        return DeviceUnit::where('team_id', auth()->user()->teams->first()->id)->get();
+        $team_id = $this->getTeamIdFromContext($context);
+
+        return DeviceUnit::where('team_id', $team_id)->get();
     }
 }
