4cd85e25b01e06d31f59e22091132dc1f4e2d984
Juan "dientuki" Farias <dientuki@gmail.com>
2024-06-24 01:02:59 -0300

    [SM-device-version] fix migratios

--- database/migrations/2024_05_05_042620_create_device_versions_parts_table.php
+++ database/migrations/2024_05_05_042620_create_device_versions_parts_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('device_parts', function (Blueprint $table) {
+        Schema::create('device_versions_parts', function (Blueprint $table) {
             $table->foreignUlid('device_version_id')->constrained();
             $table->foreignUlid('part_id')->constrained();
 
