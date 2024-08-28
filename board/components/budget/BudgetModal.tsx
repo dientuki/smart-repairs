e264d85a59b8e16d85264a4aa848a9ffe9c4ee63
Juan "dientuki" Farias <dientuki@gmail.com>
2024-08-28 01:00:47 -0300

    [SM-86a4epz4q-presupuesto] try to validate

--- board/components/budget/BudgetModal.tsx
+++ board/components/budget/BudgetModal.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { AddRow, InputCell, RemoveRow, BooleanCell } from "@/components/budget";
 import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { ActionButton } from "../form";
+import { FieldErrors, FieldValues, useFieldArray, useForm } from "react-hook-form";
 
 type BillItem = {
   description: string;
@@ -39,10 +40,21 @@ const defaultData: BillItem[] = [
 ];
 const columnHelper = createColumnHelper<BillItem>();
 
+const registerOptions = {
+  quantity: {
+    required: true
+  }
+}
+
 export const BudgetModal = () => {
   const { t } = useTranslation();
   const [data, setData] = useState(() => [...defaultData]);
   const [total, setTotal] = useState(0);
+  const { control, handleSubmit } = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'items',
+  });
 
   useEffect(() => {
     const newTotal = data
@@ -61,8 +73,9 @@ export const BudgetModal = () => {
       header: "Cantidad",
       cell: InputCell,
       meta: {
-        type: "number",
-        name: "items"
+        name: "items",
+        control: control,
+        rules: registerOptions.quantity,
       }
     }),
     columnHelper.accessor("unitPrice", {
@@ -126,57 +139,66 @@ export const BudgetModal = () => {
     },
   });
 
+  const handleRegistration = async(data: FieldValues ) => {
+    console.log(data)
+    return false;
+  }
+
+  const handleError = (error: FieldErrors<FieldValues>) => {
+    console.log('error', error)
+  };
+
   return (
     <ModalLayout minHeight="460px">
       <h2>Presupuestar equipo</h2>
-      <div>
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
+      <form onSubmit={handleSubmit(handleRegistration, handleError)}>
+        <table>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+            <tr>
+              <td colSpan={table.getCenterLeafColumns().length} align="right">
+                <AddRow table={table} />
+              </td>
             </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3} >Total</td>
+              <td colSpan={1} align="right">{total}</td>
+              <td colSpan={2}></td>
             </tr>
-          ))}
-          <tr>
-            <td colSpan={table.getCenterLeafColumns().length} align="right">
-              <AddRow table={table} />
-            </td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan={3} >Total</td>
-            <td colSpan={1} align="right">{total}</td>
-            <td colSpan={2}></td>
-          </tr>
-        </tfoot>
-      </table>
-      <div className="mt-4">
-          <ActionButton customClass=" mt-6">asdf {total}</ActionButton>
-      </div>
+          </tfoot>
+        </table>
+        <div className="mt-4">
+            <ActionButton customClass=" mt-6" type="submit">asdf {total}</ActionButton>
+        </div>
 
 
-      </div>
+      </form>
     </ModalLayout>
   )
 }
--- board/components/budget/InputCell.tsx
+++ board/components/budget/InputCell.tsx
@@ -1,27 +1,46 @@
 import { useEffect, useState } from "react";
+import { Controller } from 'react-hook-form';
+import { Input } from "@headlessui/react";
 
-export const InputCell = ({ getValue, row, column, table }) => {
+interface InputCellProps {
+  getValue: () => any;
+  row: any;
+  column: any;
+  table: any;
+}
+
+export const InputCell = ({ getValue, row, column, table }: InputCellProps) => {
   const initialValue = getValue();
-  const [value, setValue] = useState(initialValue);
+  const [val, setVal] = useState(initialValue);
+  const name = `${column.columnDef.meta?.name}.${row.id}.${column.id}`;
 
   useEffect(() => {
-    setValue(initialValue);
+    setVal(initialValue);
   }, [initialValue]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value;
-    setValue(newValue);
-
-    // Actualiza los datos en el `onChange`
+  const handleChange = (newValue: any) => {
+    setVal(newValue);
     table.options.meta?.updateData(row.index, column.id, newValue);
     console.log('Actualización en onChange');
   };
 
   return (
-    <input
-      value={value}
-      onChange={handleChange}
-      type={column.columnDef.meta?.type || "text"}
+    <Controller
+      name={name}
+      control={column.columnDef.meta.control}
+      rules={column.columnDef.meta.rules}
+      defaultValue={initialValue}
+      render={({ field }) => (
+        <Input
+          {...field}
+          type="text"
+          value={val}
+          onChange={(e) => {
+            field.onChange(e); // Actualiza react-hook-form
+            handleChange(e.target.value); // Llama a tu función personalizada
+          }}
+        />
+      )}
     />
   );
 };
