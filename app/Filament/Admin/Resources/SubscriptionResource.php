4af9e3a16d97aa6afe1883890c924fa55d223051
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-09 15:24:28 -0300

    [SM-super-admin] some admin stuff

---
+++ app/Filament/Admin/Resources/SubscriptionResource.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Filament\Admin\Resources\SubscriptionResource\Pages;
+use App\Filament\Admin\Resources\SubscriptionResource\RelationManagers;
+use App\Models\Admin\Subscription;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SubscriptionResource extends Resource
+{
+    protected static ?string $model = Subscription::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('package.name'),
+                TextColumn::make('team.name'),
+                TextColumn::make('start_at'),
+                TextColumn::make('end_at'),
+                TextColumn::make('is_active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubscriptions::route('/'),
+            'create' => Pages\CreateSubscription::route('/create'),
+            'edit' => Pages\EditSubscription::route('/{record}/edit'),
+        ];
+    }
+}
