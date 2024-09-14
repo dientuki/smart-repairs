952acca49cbdeb19c5a3457bfb4a4f32ae68a2c4
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-14 01:30:44 -0300

    [SM-card-redesign] more design

--- board/components/viewCardModal/Comment.tsx
+++ board/components/viewCardModal/Comment.tsx
@@ -65,7 +65,7 @@ function Comment({ comment, onDelete }: Props) {
         <div className="flex flex-col gap-3 ml-9">
           <div className="flex rounded-lg shadow-sm ring-1 transition duration-75 bg-white dark:bg-white/5 [&:not(:has(.fi-ac-action:focus))]:focus-within:ring-2 ring-gray-950/10 dark:ring-white/20 [&:not(:has(.fi-ac-action:focus))]:focus-within:ring-primary-600 dark:[&:not(:has(.fi-ac-action:focus))]:focus-within:ring-primary-500 overflow-hidden">
             <Textarea
-              className="block h-full w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500  dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)]  "
+              className="block h-full w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500  dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400    "
               defaultValue={commentData.comment}
               onClick={editComment}
               onChange={handleChange}
