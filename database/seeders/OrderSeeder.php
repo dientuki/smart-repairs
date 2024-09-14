63b2ffb6ec3a8117eda7cc340641dfef9c4ca6ec
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-14 20:15:59 -0300

    [SM-card-features] improve seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -15,7 +15,7 @@ class OrderSeeder extends Seeder
     public function run(): void
     {
         $customers = DB::table('customers')->get();
-        $team = DB::table('teams')->first()->id;
+        $teams = DB::table('teams')->get();
         $users = DB::table('users')->get();
         $devicesUnit = DB::table('device_units')->get();
         $devices = DB::table('devices')->get();
@@ -25,7 +25,7 @@ class OrderSeeder extends Seeder
             'observation' => 'No enciende (este es obs)',
             'customer_id' => $customers[0]->id,
             'status' => OrderStatusEnum::Budgeting,
-            'team_id' => $team,
+            'team_id' => $teams[0]->id,
             'created_by' => $users[0]->id,
             'assigned_to' => $users[3]->id,
             'device_id' => $devices[0]->id,
@@ -39,7 +39,7 @@ class OrderSeeder extends Seeder
             'observation' => 'creo que es error de usuario',
             'customer_id' => $customers[0]->id,
             'status' => OrderStatusEnum::Budgeting,
-            'team_id' => $team,
+            'team_id' => $teams[0]->id,
             'created_by' => $users[4]->id,
             'device_id' => $devices[1]->id,
             'device_unit_id' => $devicesUnit[2]->id,
@@ -52,10 +52,24 @@ class OrderSeeder extends Seeder
             'observation' => 'Sali a comprar el pan, se metio dentro de uno y le di un mordisco',
             'customer_id' => $customers[1]->id,
             'status' => OrderStatusEnum::ToDo,
-            'team_id' => $team,
+            'team_id' => $teams[0]->id,
             'created_by' => $users[4]->id,
             'device_id' => $devices[0]->id,
             'device_unit_id' => $devicesUnit[1]->id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('orders')->insert([
+            'id' => (string) Str::ulid(),
+            'observation' => 'Se lo lleve a moncho',
+            'customer_id' => $customers[2]->id,
+            'status' => OrderStatusEnum::ForBudgeting,
+            'team_id' => $teams[2]->id,
+            'created_by' => $users[2]->id,
+            'assigned_to' => $users[2]->id,
+            'device_id' => $devices[0]->id,
+            'device_unit_id' => $devicesUnit[3]->id,
             'created_at' => now(),
             'updated_at' => now(),
         ]);
