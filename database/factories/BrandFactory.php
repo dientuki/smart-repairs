f7fd5414ec7deff98c6252a389b8ebd9bbbda171
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-15 02:19:51 -0300

    [SM-card-features] fix factory

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -13,7 +13,7 @@ class BrandFactory extends Factory
     {
         return [
             'name' => $this->faker->company,
-            'hash_filename' => $this->faker->optional()->imageUrl(),
+            'hash_filename' => $this->faker->optional()->boolean ? $this->faker->imageUrl() : null,
         ];
     }
 }
