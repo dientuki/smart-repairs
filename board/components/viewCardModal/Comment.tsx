9123404545743d8511e21435a0ea595827d3a9b3
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-16 14:25:50 -0300

    [SM-card-comment] refactor comment

--- board/components/viewCardModal/Comment.tsx
+++ board/components/viewCardModal/Comment.tsx
@@ -1,53 +1,54 @@
-import { useOrderStore } from "@/store/OrderStore";
-import { Textarea } from "@headlessui/react";
-import { useState } from "react";
 import Avatar from "react-avatar";
-import { ActionButton } from "@/components/form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
-import { StyleColor } from "@/types/enums";
+import { toast } from "react-toastify";
+import { ActionButton, TextareaField } from "../form";
+import { capitalizeFirstLetter } from "@/helper/stringHelpers";
+import { useState } from "react";
 import { LockStatus } from "@/components/viewCardModal";
+import { StyleColor } from "@/types/enums";
 
 type Props = {
   comment: OrderComment;
-  onDelete: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export const Comment = ({ comment, onDelete }: Props) => {
-  const [isEditing, setIsEditing] = useState(false);
+export const Comment = ({ comment }: Props) => {
   const [commentData, setCommentData] = useState(comment);
-  const { updateCommentVisibility, updateComment } = useOrderStore();
+  const [isEditing, setIsEditing] = useState(false);
   const { t } = useTranslation();
-  let text: string = commentData.comment;
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      comment: commentData.comment,
+      ispublic: commentData.isPublic,
+    },
+  });
 
-  const toogleVisibility = () => {
-    updateCommentVisibility(comment.id, !commentData.isPublic);
-    setCommentData({
-      ...commentData,
-      isPublic: !commentData.isPublic,
-      comment: text,
-    });
+  const handleRegistration = async (data: FieldValues) => {
+
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    text = event.target.value;
+  const handleError = () => {
+    toast.error(t("toast.error.form"));
   };
 
-  const editComment = () => {
-    setIsEditing(true);
+  const registerOptions = {
+    comment: {
+      required: t("validation.required", { field: t("field.comment") }),
+    },
+    ispublic: {},
   };
 
-  const saveComment = () => {
-    setIsEditing(false);
-    updateComment(comment.id, text);
-    setCommentData({
-      ...commentData,
-      wasEdited: true,
-    });
+  const toggleVisibility = () => {
+    //setValue("ispublic", !ispublic); // Cambiar el valor de ispublic
   };
 
   return (
-    <div className='flex flex-col gap-1'>
-      <div className='flex flex-row items-center gap-2'>
+    <div className='flex flex-col gap-2'>
+      <div className='flex items-center gap-2'>
         <Avatar
           name={commentData.userName}
           round={true}
@@ -55,7 +56,6 @@ export const Comment = ({ comment, onDelete }: Props) => {
           maxInitials={2}
           src={commentData.userImage}
         />
-
         <div>
           {commentData.createdAtDate?.toDateString()}{" "}
           {commentData.createdAtDate?.toLocaleTimeString()}
@@ -64,35 +64,39 @@ export const Comment = ({ comment, onDelete }: Props) => {
         {commentData.wasEdited && <div className='text-gray-500'>Editado</div>}
 
         <LockStatus
-          toggleVisibility={toogleVisibility}
+          toggleVisibility={toggleVisibility}
           status={commentData.isPublic}
         />
       </div>
-
-      <div className='flex flex-col gap-3 ml-9'>
-        <div className='flex rounded-lg shadow-sm ring-1 transition duration-75 bg-white dark:bg-white/5 [&:not(:has(.fi-ac-action:focus))]:focus-within:ring-2 ring-gray-950/10 dark:ring-white/20 [&:not(:has(.fi-ac-action:focus))]:focus-within:ring-primary-600 dark:[&:not(:has(.fi-ac-action:focus))]:focus-within:ring-primary-500 overflow-hidden'>
-          <Textarea
-            className='block h-full w-full border-none bg-transparent px-3 py-1.5 text-base text-gray-950 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500  dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400    '
-            defaultValue={commentData.comment}
-            onClick={editComment}
-            onChange={handleChange}
-          />
-        </div>
-        <div className='flex flex-row items-center gap-3'>
+      <form
+        onSubmit={handleSubmit(handleRegistration, handleError)}
+        className='w-auto ml-9'
+      >
+        <TextareaField
+          name='comment'
+          label={t("field.comment")}
+          labelless
+          control={control}
+          rules={registerOptions.comment}
+          errors={errors}
+          rows={2}
+          placeholder={capitalizeFirstLetter(t("placeholder.comment"))}
+        />
+      </form>
+      <div className='flex flex-row items-center gap-3 w-auto ml-9'>
           {isEditing ? (
-            <ActionButton onClick={saveComment}>
+            <ActionButton onClick={toggleVisibility}>
               {t("button.save")}
             </ActionButton>
           ) : (
-            <ActionButton onClick={editComment}>
+            <ActionButton onClick={toggleVisibility}>
               {t("button.edit")}
             </ActionButton>
           )}
-          <ActionButton onClick={onDelete} style={StyleColor.Danger}>
+          <ActionButton style={StyleColor.Danger}>
             {t("button.delete")}
           </ActionButton>
         </div>
-      </div>
     </div>
   );
 };
