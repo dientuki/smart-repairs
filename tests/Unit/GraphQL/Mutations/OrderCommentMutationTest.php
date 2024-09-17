d01a4997c799b703fa785d7a6fbe3b8b9a1c9817
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-17 01:38:07 -0300

    [SM-card-comment] update tests

--- tests/Unit/GraphQL/Mutations/OrderCommentMutationTest.php
+++ tests/Unit/GraphQL/Mutations/OrderCommentMutationTest.php
@@ -3,10 +3,12 @@
 namespace Tests\Unit\GraphQL\Mutations;
 
 use App\Models\Order;
+use App\Models\OrderComment;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\Unit\GraphQL\TestCaseGraphQL;
 use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Support\Facades\Auth;
 
 class OrderCommentMutationTest extends TestCaseGraphQL
 {
@@ -75,7 +77,235 @@ class OrderCommentMutationTest extends TestCaseGraphQL
         ]);
     }
 
+    #[Test]
+    public function cannot_add_comment_with_invalid_data()
+    {
+        // Crear orden (si no existe)
+        $order = Order::factory()->create();
 
+        // Preparar los argumentos para la mutación
+        $commentData = [
+            'comment' => null,
+            'ispublic' => null,
+        ];
+
+        // Ejecutar la mutación GraphQL
+        $response = $this->graphQL('
+            mutation {
+                addComment(
+                    orderId: "' . $order->id . '",
+                    comment: {
+                        comment: "' . $commentData['comment'] . '",
+                        ispublic: ' . $this->boolToString(true) . '
+                    }
+                ) {
+                    id
+                    comment
+                    created_at
+                    is_public
+                    user_id
+                    was_edited
+                    user {
+                        name
+                        imageUrl
+                    }
+                }
+            }
+        ');
+
+        $response->assertJsonStructure([
+            'errors' => [
+                [
+                    'message',
+                    'extensions' => [
+                        'validation' => [
+                            'comment.comment',
+                        ],
+                    ],
+                ],
+            ],
+          ]);
+    }
+
+    #[Test]
+    public function update_my_comment_successfully()
+    {
+        // Crear orden (si no existe)
+        $order = Order::factory()->create();
+        $orderComment = OrderComment::factory()->create([
+            'order_id' => $order->id,
+            'team_id' => $this->team->id,
+            'user_id' => Auth::id()
+        ]);
+
+        // Preparar los argumentos para la mutación
+        $commentData = [
+            'comment' => 'Este es un nuevo comentario de prueba',
+            'ispublic' => false,
+        ];
+
+        // Ejecutar la mutación GraphQL
+        $response = $this->graphQL('
+            mutation {
+                updateComment(
+                    commentId: "' . $orderComment->id . '",
+                    comment: {
+                        comment: "' . $commentData['comment'] . '",
+                        ispublic: ' . $this->boolToString($commentData['ispublic']) . '
+                    }
+                )
+            }
+        ');
+
+        // Verificar la respuesta de GraphQL
+        $response->assertJson([
+            'data' => [
+                'updateComment' => true
+            ]
+        ]);
+
+        // Verificar que el comentario fue creado en la base de datos
+        $this->assertDatabaseHas('order_comments', [
+            'order_id' => $order->id,
+            'team_id' => $this->team->id, // Asumimos que tienes un método que obtiene el team_id
+            'comment' => $commentData['comment'],
+            'is_public' => $commentData['ispublic'],
+        ]);
+    }
+
+    #[Test]
+    public function cannot_update_other_users_comments()
+    {
+        // Crear orden (si no existe)
+        $order = Order::factory()->create();
+        $orderComment = OrderComment::factory()->create([
+            'order_id' => $order->id,
+            'team_id' => $this->team->id,
+        ]);
+
+        // Preparar los argumentos para la mutación
+        $commentData = [
+            'comment' => 'Este es un nuevo comentario de prueba',
+            'ispublic' => false,
+        ];
+
+        // Ejecutar la mutación GraphQL
+        $response = $this->graphQL('
+            mutation {
+                updateComment(
+                    commentId: "' . $orderComment->id . '",
+                    comment: {
+                        comment: "' . $commentData['comment'] . '",
+                        ispublic: ' . $this->boolToString($commentData['ispublic']) . '
+                    }
+                )
+            }
+        ');
+
+        // Verificar la respuesta de GraphQL
+        $response->assertJson([
+            'data' => [
+                'updateComment' => false
+            ]
+        ]);
+    }
+
+    #[Test]
+    public function cannot_update_comment_with_invalid_data()
+    {
+        // Crear orden (si no existe)
+        $order = Order::factory()->create();
+        $orderComment = OrderComment::factory()->create([
+            'order_id' => $order->id,
+            'team_id' => $this->team->id,
+        ]);
+
+        // Preparar los argumentos para la mutación
+        $commentData = [
+            'comment' => null,
+            'ispublic' => null,
+        ];
+
+        // Ejecutar la mutación GraphQL
+        $response = $this->graphQL('
+            mutation {
+                updateComment(
+                    commentId: "' . $orderComment->id . '",
+                    comment: {
+                        comment: "' . $commentData['comment'] . '",
+                        ispublic: ' . $this->boolToString(true) . '
+                    }
+                )
+            }
+        ');
+
+        // Verificar la respuesta de GraphQL
+        $response->assertJsonStructure([
+            'errors' => [
+                [
+                    'message',
+                    'extensions' => [
+                        'validation' => [
+                            'comment.comment',
+                        ],
+                    ],
+                ],
+            ],
+          ]);
+    }
+
+    #[Test]
+    public function delete_my_comment_successfully(){
+        $order = Order::factory()->create();
+        $orderComment = OrderComment::factory()->create([
+            'order_id' => $order->id,
+            'team_id' => $this->team->id,
+            'user_id' => Auth::id()
+        ]);
+
+        $response = $this->graphQL('
+            mutation {
+                deleteComment(commentId: "' . $orderComment->id . '")
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'deleteComment' => true
+            ]
+        ]);
+
+        // Verificar que el comentario fue eliminado de la base de datos
+        $this->assertDatabaseMissing('order_comments', [
+            'id' => $orderComment->id
+        ]);
+    }
+
+    #[Test]
+    public function cannot_delete_other_users_comments(){
+        $order = Order::factory()->create();
+        $orderComment = OrderComment::factory()->create([
+            'order_id' => $order->id,
+            'team_id' => $this->team->id,
+        ]);
+
+        $response = $this->graphQL('
+            mutation {
+                deleteComment(commentId: "' . $orderComment->id . '")
+            }
+        ');
+
+        $response->assertJson([
+            'data' => [
+                'deleteComment' => false
+            ]
+        ]);
+
+        // Verificar que el comentario fue eliminado de la base de datos
+        $this->assertDatabaseHas('order_comments', [
+            'id' => $orderComment->id
+        ]);
+    }
 
     /**
      * Test to ensure that an unauthenticated user cannot access the mutation.
