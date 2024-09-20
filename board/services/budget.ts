ff32a1fbc83af8c5e69bdf8224b2f59fca62446e
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-20 02:19:10 -0300

    [SM-card-simple-budget] fetch budget data

--- board/services/budget.ts
+++ board/services/budget.ts
@@ -5,51 +5,66 @@ import {
 } from "@/helper/graphqlHelpers";
 import { extra } from "@/helper/reduceHelpers";
 import { arrayToString } from "@/helper/stringHelpers";
+import { PackageType } from "@/types/enums";
 
-export const getInitialValues = async (orderId: string): Promise<any> => {
-  const response = await graphqlRequest(`
-      query {
-          discounts {
-              id
-              label
-              price
-              discount_type
-          }
-          services {
-              id
-              label
-              price
-          }
-          partsByOrder(orderId: "${orderId}") {
-            id
-            label
-            price
-            stock
-            image
-          }
-          budget(orderId: "${orderId}") {
-            id
-            total
-            items {
-              part_id
-              service_job_id
-              quantity
-              unit_price
-              include_in_sum
-            }
-          }
+export const getInitialValues = async (cpackage: string, orderId?: string): Promise<any> => {
+  // Construir la consulta dinámicamente según si `orderId` está presente o no
+  const query = `
+    query {
+      morph {
+        part
+        discount
+        serviceJob
       }
-  `);
+      discounts {
+        id
+        label
+        price
+        type
+      }
+      services {
+        id
+        label
+        price
+      }
+      ${orderId && cpackage !== PackageType.Basic ? `
+        partsByOrder(orderId: "${orderId}") {
+          id
+          label
+          price
+          stock
+          image
+        }
+      ` : ''}
+      ${orderId ? `
+        budget(orderId: "${orderId}") {
+          id
+          total
+          items {
+            part_id
+            service_job_id
+            quantity
+            unit_price
+            include_in_sum
+          }
+        }
+      ` : ''}
+    }
+  `;
+
+  const response = await graphqlRequest(query);
+  console.log(response);
 
   handleGraphQLErrors(response.errors);
 
   return {
     discounts: extra(response.data.discounts),
     services: extra(response.data.services),
-    parts: extra(response.data.partsByOrder),
-    budget: response.data.budget,
+    parts: orderId ? extra(response.data.partsByOrder) : [],
+    budget: orderId ? response.data.budget : null,
   };
 };
+
 
 export const updateBudget = async (
   orderId: string,
