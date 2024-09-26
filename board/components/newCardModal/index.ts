27c4131e49b767dfa304763ad5c3aed22953d9a0
Juan "dientuki" Farias <dientuki@gmail.com>
2024-09-26 16:41:34 -0300

    [SM-creater-order-step1] add wizard design

---
+++ board/components/newCardModal/index.ts
@@ -0,0 +1,1 @@
+export { TabListTab } from "./TabListTab";
---
+++ board/components/viewCardModal/OrderStatus.tsx
@@ -0,0 +1,15 @@
+import { dynamicStyles } from "@/helper/componentsHelpers";
+import { StyleColor } from "@/types/enums";
+
+interface OrderStatusProps {
+  status: string;
+}
+
+export const OrderStatus = ({ status }: OrderStatusProps) => (
+  <div
+    style={dynamicStyles(StyleColor.Info)}
+    className="text-center justify-center gap-x-1 rounded-md text-base font-medium ring-1 ring-inset px-2 py-1 bg-custom-50 text-custom-600 ring-custom-600/10 dark:bg-custom-400/10 dark:text-custom-400 dark:ring-custom-400/30"
+  >
+    {status}
+  </div>
+);
