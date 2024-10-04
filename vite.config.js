9d87ede6b10d5814744ecd5b35e8e18b5cb4fa4b
Juan "dientuki" Farias <dientuki@gmail.com>
2024-10-04 14:26:31 -0300

    [TDD] add vitest config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import 'dotenv/config';
 import path from 'path';
+import removeAttribute from '@castlenine/vite-remove-attribute';
+
+const isProduction = process.env.NODE_ENV == 'production';
 
 export default defineConfig({
     build: {
-        minify: process.env.APP_ENV === 'production' ? 'esbuild' : false,
-        cssMinify: process.env.APP_ENV === 'production',
+        minify: isProduction ? 'esbuild' : false,
+        cssMinify: isProduction,
     },
     plugins: [
         laravel({
@@ -16,6 +19,12 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        isProduction &&
+        removeAttribute({
+            extensions: ['tsx'],
+            attributes: ['data-testid'],
+            ignoreFolders: ['node_modules'],
+        }),
         react(),
     ],
     resolve: {
@@ -23,4 +32,17 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'board'),
         },
     },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['board/setupTests.js'],
+        include: ['board/**/*.test.{ts,tsx}'],  // Incluir solo los tests en la carpeta /board
+        exclude: ['dist/**', 'node_modules/**'],
+        coverage: {
+            provider: 'v8',
+            reportsDirectory: './coverage/board',
+            include: ['board/**/*.test.{ts,tsx}'],
+            exclude: ['dist/**', 'node_modules/**']
+        }
+      }
 });
