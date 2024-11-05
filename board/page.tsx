1a451443b4f908fb10e60ab76bf11ad8a8f0f6cf
Juan "dientuki" Farias <dientuki@gmail.com>
2024-11-05 01:27:48 -0300

    [SM-env] add  bugsnag

--- board/page.tsx
+++ board/page.tsx
@@ -2,25 +2,50 @@ import ReactDOM from "react-dom/client";
 import { Board } from "@/components/board";
 import Modal from "@/components/modal/Modal";
 import { ModalContainer } from "react-modal-global";
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import "./global.scss";
 import { AddButton } from "@/components/AddButton";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.min.css";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
+import Bugsnag from "@bugsnag/js";
+import BugsnagPluginReact, { BugsnagErrorBoundary } from "@bugsnag/plugin-react";
+import BugsnagPerformance from '@bugsnag/browser-performance'
+
+let ErrorBoundary: React.ComponentType | BugsnagErrorBoundary = React.Fragment;
+const apiKey = import.meta.env.VITE_BUGSNAG_API_KEY;
+
+if (apiKey && import.meta.env.prod) {
+  console.log('Iniciando Bugsnag');
+
+  // Inicializa Bugsnag solo en producción
+  Bugsnag.start({
+    apiKey: apiKey,
+    plugins: [new BugsnagPluginReact()]
+  });
+  BugsnagPerformance.start({ apiKey: apiKey });
+
+  // Obtén el plugin de React para crear el ErrorBoundary en producción
+  const bugsnagPlugin = Bugsnag.getPlugin('react');
+  ErrorBoundary = bugsnagPlugin?.createErrorBoundary(React) ?? React.Fragment;
+} else {
+  console.log('No se inicia Bugsnag', import.meta);
+}
 
 const appElement = document.getElementById("app");
 
 if (appElement) {
   ReactDOM.createRoot(appElement).render(
-    <I18nextProvider i18n={i18n}>
-      <Board />
-      <AddButton />
-      <Suspense>
-        <ModalContainer controller={Modal} />
-        <ToastContainer position='top-right' newestOnTop={false} />
-      </Suspense>
-    </I18nextProvider>,
+    <ErrorBoundary>
+      <I18nextProvider i18n={i18n}>
+        <Board />
+        <AddButton />
+        <Suspense>
+          <ModalContainer controller={Modal} />
+          <ToastContainer position='top-right' newestOnTop={false} />
+        </Suspense>
+      </I18nextProvider>
+    </ErrorBoundary>,
   );
 }
