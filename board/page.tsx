839ba1e4965c32adac5585dd04ca390a77a65f4f
Juan "dientuki" Farias <dientuki@gmail.com>
2024-11-05 02:03:26 -0300

    [SM-env] old fashion but works

--- board/page.tsx
+++ board/page.tsx
@@ -14,28 +14,27 @@ import BugsnagPluginReact, { BugsnagErrorBoundary } from "@bugsnag/plugin-react"
 import BugsnagPerformance from '@bugsnag/browser-performance'
 
 let ErrorBoundary: React.ComponentType | BugsnagErrorBoundary = React.Fragment;
-const apiKey = import.meta.env.VITE_BUGSNAG_API_KEY;
-
-if (apiKey && import.meta.env.prod) {
-  console.log('Iniciando Bugsnag');
-
-  // Inicializa Bugsnag solo en producción
-  Bugsnag.start({
-    apiKey: apiKey,
-    plugins: [new BugsnagPluginReact()]
-  });
-  BugsnagPerformance.start({ apiKey: apiKey });
-
-  // Obtén el plugin de React para crear el ErrorBoundary en producción
-  const bugsnagPlugin = Bugsnag.getPlugin('react');
-  ErrorBoundary = bugsnagPlugin?.createErrorBoundary(React) ?? React.Fragment;
-} else {
-  console.log('No se inicia Bugsnag', import.meta);
-}
-
 const appElement = document.getElementById("app");
 
 if (appElement) {
+  if (appElement.hasAttribute('data-bug')) {
+    console.log('Iniciando Bugsnag');
+    const apiKey = appElement.getAttribute('data-bug');
+
+    // Inicializa Bugsnag solo en producción
+    Bugsnag.start({
+      apiKey: apiKey,
+      plugins: [new BugsnagPluginReact()]
+    });
+    BugsnagPerformance.start({ apiKey: apiKey });
+
+    // Obtén el plugin de React para crear el ErrorBoundary en producción
+    const bugsnagPlugin = Bugsnag.getPlugin('react');
+    ErrorBoundary = bugsnagPlugin?.createErrorBoundary(React) ?? React.Fragment;
+  } else {
+    console.log('No se inicia Bugsnag');
+  }
+
   ReactDOM.createRoot(appElement).render(
     <ErrorBoundary>
       <I18nextProvider i18n={i18n}>
